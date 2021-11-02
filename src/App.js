8c7e85493c79279ad108ff3fb309b092e61b1938
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-11-02 11:19:32 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola Blockinar Challenge! <code>Soy Milena Gimenez</code>
         </p>
         <a
           className="App-link"
