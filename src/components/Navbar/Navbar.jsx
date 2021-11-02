7052bfc947495923a606158068f3ce8df13dfbdc
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-11-02 11:48:50 -0300

    Update/adding Navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+/* import {Layout} from 'antd';
+import './Footer.sass'; */
+import './Navbar.sass';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="logo"></div>
+        </nav>
+    );
+};
+
+export default Navbar;
