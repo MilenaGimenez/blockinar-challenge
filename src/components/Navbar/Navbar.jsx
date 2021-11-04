67d3e7c118013823f2718809ce2ae9eb3df38594
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-11-03 22:53:49 -0300

    Update/adding styles to the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,22 @@
 import './Footer.sass'; */
 import './Navbar.sass';
 
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <div className="logo"></div>
-        </nav>
+        <Layout className="layout">
+            <Header className="header">
+            <div className="logo" />
+            <Menu theme="dark" defaultSelectedKeys={['1']} className="menu-item">
+                <Menu.Item className="item">Hola</Menu.Item>
+                <Menu.Item className="item">Formulario</Menu.Item>
+                <Menu.Item className="item">Gráfico</Menu.Item>
+            </Menu>
+            </Header>
+        </Layout>
     );
 };
 
